eff3f5c9dc4c2b31d62bb97af2ce9acae2f70e91
tobiax100 <tobiax100@gmail.com>
2024-09-07 13:24:35 -0300

    proyecto buscador en proceso, version 1.0

---
+++ 17-PROYECTO-Buscador/js/db.js
@@ -0,0 +1,183 @@
+const autos = [
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2020,
+		precio: 30000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A4', 
+        year: 2020, 
+        precio: 40000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2015,
+		precio: 20000,
+		puertas: 2,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A6', 
+        year: 2020, 
+        precio: 35000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2016,
+		precio: 70000,
+		puertas: 4,
+		color: 'Rojo',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2015,
+		precio: 25000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Chevrolet',
+		modelo: 'Camaro',
+		year: 2018,
+		precio: 60000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Mustang', 
+        year: 2019, 
+        precio: 80000, 
+        puertas: 2, 
+        color: 'Rojo', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2020,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A3', 
+        year: 2017, 
+        precio: 55000, 
+        puertas: 2, 
+        color: 'Negro', 
+        transmision: 
+        'manual' 
+    },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2020,
+		precio: 25000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 45000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2019,
+		precio: 90000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Mustang', 
+        year: 2017, 
+        precio: 60000, 
+        puertas: 2, 
+        color: 'Negro', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2015,
+		precio: 35000,
+		puertas: 2,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2018,
+		precio: 50000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2017,
+		precio: 80000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A4', 
+        year: 2016, 
+        precio: 30000, 
+        puertas: 4, 
+        color: 'Azul', 
+        transmision: 'automatico' 
+    }
+];
+
+
+
+const marcas=['audi','BMW','Mercedes Benz','Chevrolet','Ford','Dodge'];
+const  puertas =['2','4'];
+const colores=['negro','azul','blanco','rojo']
+const transmisiones = ['automatica','manual']
+const precioMin =['20.000','30.000','40.000','50.000','60.000','70.000','80.000','90.000']
+const precioMax = ['20.000','30.000','40.000','50.000','60.000','70.000','80.000','90.000']
